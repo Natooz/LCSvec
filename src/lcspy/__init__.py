49fd84acfbdbe49da4b144559e2c328869a07ef6
Nathan Fradet <56734983+Natooz@users.noreply.github.com>
2024-03-09 14:28:23 +0100

    fix package imports

--- src/lcspy/__init__.py
+++ src/lcspy/__init__.py
@@ -1,7 +1,7 @@
 """Main LCSPy module."""
 
-from .lcspy_ext import lccs_length, lcs_length, lcs_table
+from .lcspy_ext import lccs_length, lcs, lcs_length, lcs_table
 
 __version__ = "0.0.1"
 
-__all__ = ["lccs_length", "lcs_length", "lcs_table", "lccs"]
+__all__ = ["lccs_length", "lcs", "lcs_length", "lcs_table"]
